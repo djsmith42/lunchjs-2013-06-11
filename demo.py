9a78395cd9e503c78935c33c4d3937e6f24c7002
Dave Smith <dsmith@hirevue.com>
2013-06-11 02:15:56 -0600

    remove unnecessary partial URL

--- demo.py
+++ demo.py
@@ -6,9 +6,5 @@ app = flask.Flask('Angular Slider Demo')
 def _root():
     return open("index.html").read()
 
-@app.route("/partials/<filename>")
-def _partial(filename):
-    return open("partials/{0}".format(filename)).read()
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
