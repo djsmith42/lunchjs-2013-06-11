b65514d0551c5c4f671a08c200e4783a57a86d40
Dave Smith <dsmith@hirevue.com>
2013-06-11 02:53:27 -0600

    Super simple controller

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,4 @@
 angular.module("MyApp", []).
 controller("MyController", function($scope) {
-    $scope.imageToShow = 1;
-
-    $scope.showImage = function(number) {
-        $scope.imageToShow = number;
-    }
+    // Ain't nothin' here.
 })
